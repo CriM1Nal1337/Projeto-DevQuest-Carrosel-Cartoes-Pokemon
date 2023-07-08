72e237af50e98870f98cf3b1de6ffb03e63c3b1e
Elton Ferreira Borges <eltonfborges123@gmail.com>
2023-07-08 10:37:58 -0300

    carrossel infinito

--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,33 @@
-const btnAvancar = document.getElementById('btn-avancar')
-const btnVoltar = document.getElementById('btn-voltar')
-const cartoes = document.querySelectorAll('.cartao')
-let cartaoAtual = 0
-
-btnAvancar.addEventListener('click', function () {
-    if(cartaoAtual === cartoes.length - 1) return
-    cartaoAtual++
-    cartoes[cartaoAtual].classList.add('selecionado')
-    const cartaoSelecionado = document.querySelector('.selecionado')
-    cartaoSelecionado.classList.remove('selecionado')
-})
-
-btnVoltar.addEventListener('click', function () {
-    if(cartaoAtual === 0) return
-    const cartaoSelecionado = document.querySelector('.selecionado')
-    cartaoSelecionado.classList.remove('selecionado')
-    cartaoAtual--
-    cartoes[cartaoAtual].classList.add('selecionado')
-    
-})
+document.addEventListener("DOMContentLoaded", function() {
+    const btnAvancar = document.getElementById("btn-avancar");
+    const btnVoltar = document.getElementById("btn-voltar");
+    const cartoes = document.querySelectorAll(".cartao");
+  
+    let cartaoAtual = 0;
+  
+    function exibirCartao(cartaoAtual) {
+      cartoes.forEach(function(cartao) {
+        cartao.classList.remove("selecionado");
+      });
+      cartoes[cartaoAtual].classList.add("selecionado");
+    }
+  
+    function avancarCartao() {
+      cartaoAtual++;
+      if (cartaoAtual >= cartoes.length) {
+        cartaoAtual = 0;
+      }
+      exibirCartao(cartaoAtual);
+    }
+  
+    function voltarCartao() {
+      cartaoAtual--;
+      if (cartaoAtual < 0) {
+        cartaoAtual = cartoes.length - 1;
+      }
+      exibirCartao(cartaoAtual);
+    }
+  
+    btnAvancar.addEventListener("click", avancarCartao);
+    btnVoltar.addEventListener("click", voltarCartao);
+  });
